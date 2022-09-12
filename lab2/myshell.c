8a18cab9e2764fd296bd3adbf14607844768d72d
viki0526 <77221209+viki0526@users.noreply.github.com>
2022-09-12 11:46:24 +0800

    Add files via upload

---
+++ lab2/myshell.c
@@ -0,0 +1,42 @@
+/**
+ * CS2106 AY22/23 Semester 1 - Lab 2
+ *
+ * This file contains function definitions. Your implementation should go in
+ * this file.
+ */
+
+
+#include "myshell.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void my_init(void) {
+
+    // Initialize what you need here
+
+ 
+}
+
+
+
+void my_process_command(size_t num_tokens, char **tokens) {
+
+    // Your code here, refer to the lab document for a description of the
+    // arguments
+
+ 
+}
+
+
+
+void my_quit(void) {
+
+    // Clean up function, called after "quit" is entered as a user command
+
+
+
+    printf("\nGoodbye\n");
+}
